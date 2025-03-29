5aa114c3fea15673104e64045c6243090e5d87e9
Cory Cole <coryPatrick.js@gmail.com>
2025-03-29 19:02:13 -0400

    accept player move and display.

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,17 @@ function App() {
   const [computerMove, setComputerMove] = useState<Move>(() =>
     getRandomMove(MOVES)
   );
+  const [playerMove, setPlayerMove] = useState<Move | null>(null);
 
-  return <div className="App">Computer Move: {computerMove}</div>;
+  return (
+    <div className="App">
+      <div>Computer Move: {computerMove}</div>
+      <div>Player Move: {playerMove ?? "awaiting player move"} </div>
+      <button onClick={() => setPlayerMove("rock")}>Rock</button>
+      <button onClick={() => setPlayerMove("paper")}>Paper</button>
+      <button onClick={() => setPlayerMove("scissors")}>Rock</button>
+    </div>
+  );
 }
 
 export default App;
